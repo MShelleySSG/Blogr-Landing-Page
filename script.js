a129f068da8f5c9a1ae61a1f3aaa303bdce06e88
MartinShelley <martin.shelley91@gmail.com>
2021-05-25 21:01:00 +0100

    finishing off mobile view

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const hamburgerIcon = document.querySelector('.hamburgerIcon');
 const navContainer = document.querySelector('.navContainer');
+const arrows = document.querySelectorAll('.arrow-image');
+const navHeading = document.querySelectorAll('.mainNavHeading > a');
+const dropdowns = document.querySelectorAll('.dropdownMenu');
+
 
 hamburgerIcon.addEventListener('click', () => {
     if (navContainer.clientHeight == 0){
@@ -12,6 +16,24 @@ hamburgerIcon.addEventListener('click', () => {
     }
 });
 
+for (let i = 0; i < dropdowns.length; i++){
+    navHeading[i].addEventListener('click', function(){
+        if(dropdowns[i].style.display == "block"){
+            dropdowns[i].style.display = "none";
+            this.lastChild.style.transform = "rotate(0deg)";
+            this.lastChild.style.transition = "0.6s";
+            dropdowns[i].parentElement.style.marginBottom = "0px";
+            
+        }
+        else{
+            dropdowns[i].style.display = "block";
+            this.lastChild.style.transform = "rotate(180deg)";
+            this.lastChild.style.transition = "0.6s";
+            let marginBottom = dropdowns[i].children.length * 40;
+            dropdowns[i].parentElement.style.marginBottom = `${marginBottom}px`;
+        }
+    });
+}
 
 
 
